eb2ec4d7d39fa0d29a0bb4ef2b7e449889bcf2d0
Devin Smaldore <devinsmaldore@devins-macbook-pro.local>
2017-03-15 12:21:07 -0400

    Chapter 12

---
+++ Homepwner/ItemCell.swift
@@ -0,0 +1,15 @@
+//
+//  itemCell.swift
+//  Homepwner
+//
+//  Created by Devin Smaldore on 3/9/17.
+//  Copyright © 2017 Big Nerd Ranch. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var serialNumberLabel: UILabel!
+    @IBOutlet var valueLabel: UILabel!
+}
